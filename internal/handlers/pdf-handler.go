0fe692413512b4b437e36e1232f2fa5cf44c5a7c
Basty <banny444666@gmail.com>
2025-01-31 16:54:09 +0300

    feat: асинхронная обработка файлов

--- internal/handlers/pdf-handler.go
+++ internal/handlers/pdf-handler.go
@@ -11,11 +11,11 @@ import (
 	"pdf-microservice/internal/pdf"
 	"pdf-microservice/internal/save/local"
 	"pdf-microservice/internal/save/s3-storage"
+	"sync"
 )
 
 func GeneratePDFHandler(cfg *options.Config, s3Client *minio.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		var err error
 		var requestData []models.RequestData
 		if err = json.NewDecoder(r.Body).Decode(&requestData); err != nil {
@@ -24,53 +24,65 @@ func GeneratePDFHandler(cfg *options.Config, s3Client *minio.Client) http.Handle
 		}
 
 		resultFiles := make(map[string]*models.File)
-		var pdfKey string
-		var s3Key string
+		var mu sync.Mutex
 		response := make(map[string]string)
 
+		var wg sync.WaitGroup
+		sem := make(chan struct{}, 10) // Ограничение на 10 горутин
+
 		for _, adult := range requestData[0].User.Adults {
+			wg.Add(1)
+			go func(adult models.Adult) {
+				defer wg.Done()
+				sem <- struct{}{} // Семафор
+				defer func() { <-sem }()
 
-			file := models.NewFile(requestData[0].Ticket.ID, adult.FirstName, adult.LastName, cfg)
-			file.Bytes, err = pdf.GeneratePDF(requestData[0].Ticket, adult, file.S3URL)
-			if err != nil {
-				log.Printf("Error generating PDF: %v", err)
-				http.Error(w, "Failed to generate PDF", http.StatusInternalServerError)
-			}
-			resultFiles[file.Filename] = file
-
-			if cfg.Api.LocalSave {
-				err = local.SaveLocalPDF(cfg, file.Filename, file.Bytes)
+				file := models.NewFile(requestData[0].Ticket.ID, adult.FirstName, adult.LastName, cfg)
+				file.Bytes, err = pdf.GeneratePDF(requestData[0].Ticket, adult, file.S3URL)
 				if err != nil {
-					log.Printf("Failed to save PDF locally: %v", err)
-					http.Error(w, "Failed to save PDF locally", http.StatusBadRequest)
+					log.Printf("Error generating PDF for %s %s: %v", adult.FirstName, adult.LastName, err)
+					return
 				}
-				pdfKey = fmt.Sprint(adult.FirstName + "-" + adult.LastName + "-local-pdf")
-				response[pdfKey] = file.Filename
-				log.Printf("Successfully generated and saved PDF locally: %s", file.Filename)
-			}
 
-			err = s3_storage.UploadFile(cfg, s3Client, file.Filename, file.Bytes)
-			if err != nil {
-				log.Printf("Failed to upload to S3: %v", err)
-				http.Error(w, "Failed to upload to S3", http.StatusBadRequest)
-			}
+				mu.Lock()
+				resultFiles[file.Filename] = file
+				mu.Unlock()
 
-			s3Key = fmt.Sprint(adult.FirstName + "-" + adult.LastName + "-s3-storage-url")
+				if cfg.Api.LocalSave {
+					err = local.SaveLocalPDF(cfg, file.Filename, file.Bytes)
+					if err != nil {
+						log.Printf("Failed to save PDF locally for %s %s: %v", adult.FirstName, adult.LastName, err)
+						return
+					}
+					pdfKey := fmt.Sprint(adult.FirstName + "-" + adult.LastName + "-local-pdf")
+					mu.Lock()
+					response[pdfKey] = file.Filename
+					mu.Unlock()
+					log.Printf("Successfully generated and saved PDF locally: %s", file.Filename)
+				}
 
-			response[s3Key] = file.S3URL
+				err = s3_storage.UploadFile(cfg, s3Client, file.Filename, file.Bytes)
+				if err != nil {
+					log.Printf("Failed to upload to S3 for %s %s: %v", adult.FirstName, adult.LastName, err)
+					return
+				}
+				s3Key := fmt.Sprint(adult.FirstName + "-" + adult.LastName + "-s3-storage-url")
+				mu.Lock()
+				response[s3Key] = file.S3URL
+				mu.Unlock()
+			}(adult)
 		}
 
-		if err == nil {
-			log.Printf("Successfully generated and uploaded PDF to %s: %s", cfg.S3.BucketName, response)
-		}
+		wg.Wait()
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-
 		if err = json.NewEncoder(w).Encode(response); err != nil {
-			log.Printf("Failed to encode json response: %v", err)
-			http.Error(w, "Failed to encode json response", http.StatusBadRequest)
-
+			log.Printf("Failed to encode JSON response: %v", err)
+			http.Error(w, "Failed to encode JSON response", http.StatusBadRequest)
+			return
 		}
+
+		log.Printf("Successfully generated and uploaded PDFs to %s: %v", cfg.S3.BucketName, response)
 	}
 }
